8fdbcddea55856f87ae9ce2b79b53ef7611ba013
Ian Lander <ian@landex.dev>
2025-04-19 13:43:39 -0700

    Updated footer to include donate button.

--- wuzzle/src/wuzzle.jsx
+++ wuzzle/src/wuzzle.jsx
@@ -232,7 +232,36 @@ function Wuzzle() {
           rel="noopener noreferrer"
         >
           Landex Development
-        </a>
+        </a>{" "}
+        {" | "}
+        <form
+          action="https://www.paypal.com/donate"
+          method="post"
+          target="_left"
+          style={{
+            display: "inline-block",
+            marginLeft: "8px",
+          }}
+        >
+          <input type="hidden" name="business" value="WHEJBZ4SV3HB4" />
+          <input type="hidden" name="no_recurring" value="0" />
+          <input type="hidden" name="currency_code" value="USD" />
+          <input
+            type="image"
+            src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+            border="0"
+            name="submit"
+            title="PayPal - The safer, easier way to pay online!"
+            alt="Donate with PayPal button"
+          />
+          <img
+            alt=""
+            border="0"
+            src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+            width="1"
+            height="1"
+          />
+        </form>
       </footer>
     </div>
   );
